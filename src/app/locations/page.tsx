58f99eb2f9ed912f579da65f72b5964d085ad584
Gabriel Paiva <gabriel.paiva@pipefy.com>
2024-06-21 23:20:16 -0300

    feat: create locations page

--- src/app/locations/page.tsx
+++ src/app/locations/page.tsx
@@ -3,15 +3,83 @@
 import Button from "@/components/Button";
 import PrivateHeader from "@/components/Headers/PrivateHeader";
 import Heading from "@/components/Typography/Heading";
-import { Flex, Form, TextField } from "@adobe/react-spectrum";
+import {
+  ActionButton,
+  Cell,
+  Column,
+  Content,
+  Dialog,
+  DialogTrigger,
+  Divider,
+  Flex,
+  Row,
+  TableBody,
+  TableHeader,
+  TableView,
+  Text,
+  Heading as SpectrumHeading,
+} from "@adobe/react-spectrum";
 
 export default function Locations() {
   return (
     <>
       <PrivateHeader />
       <main className="flex min-h-screen flex-col items-center gap-20 p-20">
-        <Flex direction="column">
+        <Flex height="100%" width="100%" direction="column" gap="size-150">
           <Heading>Seus Imóveis</Heading>
+
+          <DialogTrigger isDismissable type="modal">
+            <ActionButton>Adicionar novo imóvel</ActionButton>
+            <Dialog>
+              <SpectrumHeading>Modal</SpectrumHeading>
+              <Divider />
+              <Content>
+                <Text>This is a modal.</Text>
+              </Content>
+            </Dialog>
+          </DialogTrigger>
+
+          <TableView flex aria-label="Locations table">
+            <TableHeader>
+              <Column>Nome</Column>
+              <Column>Endereço</Column>
+              <Column>Ação</Column>
+            </TableHeader>
+            <TableBody>
+              <Row>
+                <Cell>Games</Cell>
+                <Cell>File folder</Cell>
+                <Cell>
+                  <DialogTrigger isDismissable type="modal">
+                    <ActionButton>Ver imóvel</ActionButton>
+                    <Dialog>
+                      <SpectrumHeading>Imóvel X</SpectrumHeading>
+                      <Divider />
+                      <Content>
+                        <Text>This is a modal.</Text>
+                      </Content>
+                    </Dialog>
+                  </DialogTrigger>
+                </Cell>
+              </Row>
+              <Row>
+                <Cell>Games 2</Cell>
+                <Cell>File folder 2</Cell>
+                <Cell>
+                  <DialogTrigger isDismissable type="modal">
+                    <ActionButton>Ver imóvel</ActionButton>
+                    <Dialog>
+                      <SpectrumHeading>Imóvel X</SpectrumHeading>
+                      <Divider />
+                      <Content>
+                        <Text>This is a modal.</Text>
+                      </Content>
+                    </Dialog>
+                  </DialogTrigger>
+                </Cell>
+              </Row>
+            </TableBody>
+          </TableView>
         </Flex>
       </main>
     </>
