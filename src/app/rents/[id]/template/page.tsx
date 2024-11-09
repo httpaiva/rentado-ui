106ce321cf6d7a25e6117d20a8ecc87d7886166d
Gabriel Paiva <gabriel.paiva@pipefy.com>
2024-11-08 21:30:52 -0300

    improvement: add feedback for users without templates

--- src/app/rents/[id]/template/page.tsx
+++ src/app/rents/[id]/template/page.tsx
@@ -14,6 +14,7 @@ import {
   FormLabel,
   FormDescription,
   FormMessage,
+  H3,
 } from "@/components";
 import { Template } from "@/types/Template";
 import { useParams, useRouter } from "next/navigation";
@@ -79,7 +80,7 @@ function Rents() {
   const form = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
     defaultValues: {
-      template: '',
+      template: "",
     },
   });
 
@@ -91,43 +92,55 @@ function Rents() {
   }, [templates]);
 
   const onSubmit = async (values: z.infer<typeof schema>) => {
-    console.log({values})
     router.push(`/templates/preview/${values.template}?rent_id=${params.id}`);
-    
   };
 
   return (
     <PageWithHeaderAndSidebar>
-      <main className="flex min-h-screen flex-col items-center gap-20 p-20">
-        <H2>Gerar documento para {rent?.location?.name}</H2>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-8 flex flex-col justify-center items-center"
-          >
+      <main className="flex min-h-screen flex-col items-center gap-16 p-16">
+        <div className="flex flex-col gap-4 items-center">
+          <H2>Gerar documento</H2>
+          <H3>
+            Para {rent?.renter?.firstName} no imóvel {rent?.location?.name}
+          </H3>
+        </div>
+        {!templates.length && (
+          <div className="flex flex-col gap-4">
+            <p>Não há templates cadastrados</p>
+            <Button onClick={() => router.push("/templates/new")}>
+              Criar template
+            </Button>
+          </div>
+        )}
+        {!!templates.length && (
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-8 flex flex-col justify-center items-center"
+            >
+              <FormField
+                control={form.control}
+                name="template"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Template</FormLabel>
+                    <SelectField
+                      {...field}
+                      defaultValue={field.value}
+                      onValueChange={field.onChange}
+                      items={mappedTemplates || []}
+                      placeholder="Selecione um template"
+                    />
+                    <FormDescription>Selecione um template</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="template"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Template</FormLabel>
-                  <SelectField
-                    {...field}
-                    defaultValue={field.value}
-                    onValueChange={field.onChange}
-                    items={mappedTemplates || []}
-                    placeholder="Selecione um template"
-                  />
-                  <FormDescription>Selecione um template</FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <Button type="submit">Gerar documento</Button>
-          </form>
-        </Form>
+              <Button type="submit">Gerar documento</Button>
+            </form>
+          </Form>
+        )}
       </main>
     </PageWithHeaderAndSidebar>
   );
